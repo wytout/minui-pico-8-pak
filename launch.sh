556df84daf965e2f57a87e3405adb06c4e8cb408
wytout <tawspam@icloud.com>
2025-06-18 10:59:27 -0400

    Update launch.sh - add my355

--- launch.sh
+++ launch.sh
@@ -207,7 +207,7 @@ launch_cart() {
 }
 
 verify_platform() {
-    allowed_platforms="rg35xxplus tg5040"
+    allowed_platforms="rg35xxplus tg5040 my355"
     if ! echo "$allowed_platforms" | grep -q "$PLATFORM"; then
         show_message "$PLATFORM is not a supported platform" 2
         return 1
